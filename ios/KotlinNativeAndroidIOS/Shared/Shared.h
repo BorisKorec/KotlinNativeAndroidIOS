8affd1d08d7a1a7ce2ddb78bf2f39a560546a47d
Boris Korec <boris.korec@avast.com>
2019-06-12 12:05:25 +0200

    in iOS project add Cocoa Touch Framework, name it Shared, add run script into build phases of this framework

---
+++ ios/KotlinNativeAndroidIOS/Shared/Shared.h
@@ -0,0 +1,19 @@
+//
+//  Shared.h
+//  Shared
+//
+//  Created by Boris Korec on 12/06/2019.
+//  Copyright © 2019 Boris Korec. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Shared.
+FOUNDATION_EXPORT double SharedVersionNumber;
+
+//! Project version string for Shared.
+FOUNDATION_EXPORT const unsigned char SharedVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Shared/PublicHeader.h>
+
+
