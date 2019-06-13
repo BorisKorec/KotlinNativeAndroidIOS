74278d572b3bcca97ff2bb81b6c71e5ab4706d9b
Boris Korec <boris.korec@avast.com>
2019-06-13 08:45:04 +0200

    use Placeholder Api in android MainActivity

--- app/src/main/java/com/avast/kotlinnativeandroidios/MainActivity.kt
+++ app/src/main/java/com/avast/kotlinnativeandroidios/MainActivity.kt
@@ -1,14 +1,32 @@
 package com.avast.kotlinnativeandroidios
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers.Main
+import kotlinx.coroutines.launch
+import kotlin.coroutines.CoroutineContext
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), CoroutineScope {
+
+    override val coroutineContext: CoroutineContext
+        get() = Main
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         findViewById<TextView>(R.id.txtSharedText).setText(getSharedModuleString())
+
+
+        val api = PlaceholderApi()
+
+        api.callGetPost(1) {
+            Log.d("MainActivity", "data: " + it)
+            launch {
+                findViewById<TextView>(R.id.txtSharedText).setText(it)
+            }
+        }
     }
 }
